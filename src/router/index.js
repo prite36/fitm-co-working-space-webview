3f112cd0de000c2235fb5b6b4235baa2386fbba2
prite36 <spritelemon36@hotmail.com>
2017-11-03 01:17:18 +0700

    simple Route

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import Register from '@/components/Register'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: HelloWorld
+    },
+    {
+      path: '/user/:senderID',
+      name: 'Register',
+      component: Register
     }
   ]
 })
