cded042c0e4a6ef0e4a5d964c63fcb857e536605
DESKTOP-V02D53O\champ <champtnpc@gmail.com>
2017-11-08 09:59:38 +0700

    fix data personelregister

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,33 +1,78 @@
 <template>
   <div class="hello">
-    <div class="">
+    <!-- <div class="inputregister">
       Name <input type="text" name="name" value="" v-model="name">
+      <br>
       Email <input type="text" name="email" value="" v-model="email">
       <button type="button" name="button" @click="postPost()">Submit</button>
-    </div>
+    </div> -->
+<div class="field">
+  <div class="control has-icons-left has-icons-right">
+    <form >
+      <label class="label">First Name</label>
+      <input class="input" type="text" v-model="firstName" required>
+      <br><br>
+      <label class="label">Last Name</label>
+      <input class="input" type="text" v-model="lastName" required>
+      <br><br>
+      <label class="label">Username</label>
+      <input class="input" type="text" v-model="userName" required>
+      <br><br>
+      <label class="label">Email</label>
+      <input class="input" type="text" placeholder="example@mail.com" v-model="email" required>
+      <br><br>
+      <label class="label">Phone Number</label>
+      <input class="input" type="text" placeholder="089999xxxx" v-model="phoneNumber" required>
+      <br><br>
+      <label class="label">Birth Day</label>
+      <input class="input" type="date" placeholder="" v-model="birtday" required>
+      <br><br>
+      <label class="label">Gender</label>
+      <div class="field">
+        <div class="control">
+          <label class="radio">
+            <input type="radio" v-model="gender">
+            male
+          </label>
+          <label class="radio">
+            <input type="radio" v-model="gender">
+            female
+          </label>
+        </div>
+      </div>
+      <center>
+      <button class="button is-link" @click="postPost()">Submit</button>
+      </center>
+    </form>
+  </div>
+</div>
   </div>
 </template>
 
 <script>
-import firebase from 'firebase'
+// import firebase from 'firebase'
 import axios from 'axios'
-var config = {
-  apiKey: 'AIzaSyAE2rQQye4hlRpDqAWirvyaaCExiaWA8DY',
-  authDomain: 'fitm-coworkingspace.firebaseapp.com',
-  databaseURL: 'https://fitm-coworkingspace.firebaseio.com',
-  projectId: 'fitm-coworkingspace',
-  storageBucket: 'fitm-coworkingspace.appspot.com',
-  messagingSenderId: '181239315787'
-}
-firebase.initializeApp(config)
-var db = firebase.database()
-console.log(db)
+// var config = {
+//   apiKey: 'AIzaSyAE2rQQye4hlRpDqAWirvyaaCExiaWA8DY',
+//   authDomain: 'fitm-coworkingspace.firebaseapp.com',
+//   databaseURL: 'https://fitm-coworkingspace.firebaseio.com',
+//   projectId: 'fitm-coworkingspace',
+//   storageBucket: 'fitm-coworkingspace.appspot.com',
+//   messagingSenderId: '181239315787'
+// }
+// firebase.initializeApp(config)
+// var db = firebase.database()
 export default {
-  name: 'HelloWorld',
+  name: 'Register',
   data () {
     return {
-      name: '',
-      email: ''
+      firstName: '',
+      lastName: '',
+      userName: '',
+      email: '',
+      phoneNumber: '',
+      birtday: '',
+      gender: ''
     }
   },
   methods: {
@@ -36,8 +81,13 @@ export default {
         body: {
           status: this.$route.params.status,
           senderID: this.$route.params.senderID,
-          name: this.name,
-          email: this.email
+          firstName: data.body.firstName,
+          lastName: this.lastName,
+          userName: this.userName,
+          email: this.email,
+          phoneNumber: this.phoneNumber,
+          birtday: this.birtday,
+          gender: this.gender
         }
       })
       .then(response => {
@@ -53,21 +103,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.field {
+  margin-left: 2%;
+  margin-right: 2%;
 }
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+.label {
+  font-size: 16px;
 }
 </style>
