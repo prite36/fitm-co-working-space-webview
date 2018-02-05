5caab037f19deb7952ec46d770b1a1aebd6f4d31
SPRITELEMON\Spritelemon <spritelemon36@hotmail.com>
2018-02-05 17:45:02 +0700

    วางโครง timeline

---
+++ src/components/BookingTimeline.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="BookingTimeline">
+    <timeline :data="[['Washington', '1789-04-29', '1797-03-03'],['Adams', '1797-03-03', '1801-03-03']]"></timeline>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      email: '',
+      name: '',
+      phone: '',
+      url: ''
+    }
+  },
+  methods: {
+    validateForm (scope) {
+      this.$validator.validateAll(scope).then((result) => {
+        if (result) {
+          // eslint-disable-next-line
+          alert('Form Submitted!')
+        }
+      })
+    }
+  }
+  // computed: {
+  //   name () {
+  //     return `${this.first_name} ${this.last_name}`
+  //   }
+  // }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,25 @@
 import Vue from 'vue'
 import VeeValidate from 'vee-validate'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import BookingTimeline from '@/components/BookingTimeline'
 import Register from '@/components/Register'
 import Booking from '@/components/Booking'
 import ReBooking from '@/components/ReBooking'
+import Chartkick from 'chartkick'
+import VueChartkick from 'vue-chartkick'
+/* eslint-disable */
+import Chart from 'chart.js'
+/* eslint-enable */
 Vue.use(Router)
 Vue.use(VeeValidate)
+Vue.use(VueChartkick, { Chartkick })
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      name: 'BookingTimeline',
+      component: BookingTimeline
     },
     {
       path: '/register/:senderID/:status',
