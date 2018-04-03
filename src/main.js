49281e77ee1f03b8a4ef642f27094f0fad5940c2
SPRITELEMON\Spritelemon <spritelemon36@hotmail.com>
2018-04-04 04:42:30 +0700

    เปลี่ยน start rate ไปอยู่ใน card  + ติดตั้ง element.eleme

--- src/main.js
+++ src/main.js
@@ -7,7 +7,10 @@ import router from './router'
 import VeeValidate from 'vee-validate'
 import Vuetify from 'vuetify'
 import VueFire from 'vuefire'
+import {Rate} from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en'
 
+Vue.use(Rate, { locale })
 Vue.use(Vuetify)
 Vue.use(VueFire)
 Vue.config.productionTip = false
